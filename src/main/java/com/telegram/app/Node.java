651a1f2c778e8db54684d587fa2e7d21183bfb1a
Zowie Haugaard <zdh27@nau.edu>
2017-02-09 18:52:50 -0700

    added a new NodeThread class, building out node more

--- src/main/java/com/telegram/app/Node.java
+++ src/main/java/com/telegram/app/Node.java
@@ -1,14 +1,44 @@
 package com.telegram.app;
 
-import java.net.Socket;
+import java.net.*;
+import java.io.*;
 
-public class Node{
+public class Node implements Runnable{
 
-  Socket sock;
-  String name;
-  Node[] connections;
+  private ServerSocket  serve;
+  private Socket        sock;
+  private Thread        thread;
+  private String        name;
+  private NodeThread    connections[];
+  private int           connectionCount;
 
-  private boolean connect(Node){
+
+  public Node(int port){
+    try {
+      serve = new ServerSocket(port);
+      start();
+      System.out.println("Node started: " + serve)
+    }
+    catch(IOException ioe)
+      {  System.out.println(ioe.getMessage());
+      }
+  }
+
+  public void run(){
+    while(thread){
+      socket = serve.accept();;
+      connections[connectionCount] = new NodeThread(this, socket);
+    }
+  }
+
+  public void start(){
+    if(!thread){
+      thread = new Thread(this);
+      thread.start()
+    }
+  }
+
+  private boolean connect(Node node){
     // creates a connection to another node
   }
 
---
+++ src/main/java/com/telegram/app/NodeThread.java
@@ -0,0 +1,12 @@
+package com.telegram.app;
+
+import java.net.*;
+import java.io.*;
+
+
+public class NodeThread extends Thread
+{
+  private DataInputStream  messageIn  =  null;
+  private DataOutputStream messageOut = null;
+
+}
